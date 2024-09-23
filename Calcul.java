8ccc6e33d92933690e85efa78ce17b1a0eac14b5
benjaouhara <h.benjaouhara@gmail.com>
2024-09-23 22:25:49 +0200

    first validation

---
+++ Calcul.java
@@ -0,0 +1,15 @@
+ 
+ public class Calcul
+ {
+	 
+	 public static void main (String args[])
+	 {
+		 int m=4;
+		 int n= 6;
+		 m=++n;
+		 System.out.println(m);
+		 System.out.println(n);
+		  System.out.println(m+n);
+	 }
+ }
+ 
